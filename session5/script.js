ecb5d620326401b275a1a3f47cf884dcbc5abef5
Jean-Patrick Debaëne <jp.debaene@gmail.com>
2023-04-15 12:05:00 +0200

    section 5

---
+++ session5/script.js
@@ -0,0 +1,14 @@
+//alert("Hello World !");
+
+//confirm("Souhaitez-vous confirmer ?");
+
+// if (confirm("Souhaitez-vous confirmer ? ")) {
+//     // Oui
+//     alert("Ok");
+// } else {
+//     // Non
+//     alert("Annulation");
+// }
+
+const age = prompt("Entrez votre age : ");
+alert("Bonjour, vous avez " + age + " ans.")
