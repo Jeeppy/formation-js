30092db70155e15a15864a75680fdcbceca40883
Jean-Patrick Debaëne <jp.debaene@gmail.com>
2023-04-16 22:01:34 +0200

    section 8: conditions and exceptions

---
+++ session8/challenge1.js
@@ -0,0 +1,11 @@
+let age = 40;
+
+if (age < 18) {
+    console.log("Vous n'êtes pas majeur.");
+}
+else if (age < 21) {
+    console.log("Vous êtes majeur en France");
+}
+else {
+    console.log("Voums êtes majeur partout, à vous les casinos !");
+}
---
+++ session8/challenge2.js
@@ -0,0 +1,3 @@
+let x = 5;
+
+x > 3 ? console.log('X est supérieur à trois') : console.log('X n\'est pas supérieur à trois');
